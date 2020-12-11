8496a82a9d634a71bc8aa37facb4b3169c5a1a29
David Hamicev <davidhamicev@mail.ru>
2020-12-11 17:04:09 +0300

    more OOP

--- Week-2/main.js
+++ Week-2/main.js
@@ -10,15 +10,17 @@
 // Желательно использовать ООП и прототипы. Если сложно, можно какую-то маленькую часть сделать на прототипах.
 // За неделю возможно проблематично будет сделать, поэтому таких жестких сроков нет)
 // Если совсем не понятно как это делать или есть вопросы - пиши
+const PLAYER_X = 'X'
+const PLAYER_O = 'O'
 
-const Game = {
-	gameTable: document.querySelector('.game__table'),
-	currentPlayerName: document.querySelector('.game__player-cur'),
-	gameResult: document.querySelector('.game__result'),
-	restartButton: document.querySelector('.game__restart'),
-	currentPlayer: (Math.random() > 0.5) ? 'X' : 'O',
-	gameState: new Array(9).fill(null),
-	winOptions: [
+function Game(gameBoard) {
+	this.gameTable = gameBoard.querySelector('.game__table'),
+	this.currentPlayerName = gameBoard.querySelector('.game__player-cur'),
+	this.gameResult = gameBoard.querySelector('.game__result'),
+	this.restartButton = gameBoard.querySelector('.game__restart'),
+	this.currentPlayer = (Math.random() > 0.5) ? PLAYER_X : PLAYER_O,
+	this.gameState = new Array(9).fill(null),
+	this.winOptions = [
 		[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
@@ -28,20 +30,19 @@ const Game = {
 		[0, 4, 8],
 		[2, 4, 6]
 	],
-	init() {
+	this.init = function() {
 		this.setCurrentPlayerName();
-		this.gameTable.addEventListener('click', Game.handleTurn.bind(Game));
-		this.restartButton.addEventListener('click', Game.handleRestart.bind(Game));
+		this.gameTable.addEventListener('click', this.handleTurn.bind(this));
+		this.restartButton.addEventListener('click', this.handleRestart.bind(this));
 	},
-	//
-	handleTurn() {
+	this.handleTurn = function(event) {
 		const currentCell = event.target;
 		const currentCellId = currentCell.dataset.cellId;
 	
 		if (!currentCell.classList.contains('game__cell'))
-			return false;
+			return;
 
-		if (this.currentPlayer === 'X') {
+		if (this.currentPlayer === PLAYER_X) {
 			currentCell.classList.add('-tic');
 			this.gameState[currentCellId] = this.currentPlayer;
 		}
@@ -52,41 +53,37 @@ const Game = {
 		
 		if (this.isWin()) {
 			this.finisnigGame();
-			return false;
+			return;
 		}
 		else if (!this.gameState.includes(null)) {
 			this.gameResult.textContent = `It's a draw!`;
-			return false;
+			return;
 		}
 
-		this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+		this.currentPlayer = this.currentPlayer === PLAYER_X ? PLAYER_O : PLAYER_X;
 		this.setCurrentPlayerName();
 	},
 
-	setCurrentPlayerName() {
-		this.currentPlayer === 'X' ? this.currentPlayerName.textContent = 'X' : this.currentPlayerName.textContent = 'O';
+	this.setCurrentPlayerName = function() {
+		this.currentPlayer === PLAYER_X ? this.currentPlayerName.textContent = PLAYER_X : this.currentPlayerName.textContent = PLAYER_O;
 	},
 
-	handleRestart() {
+	this.handleRestart = function() {
 		this.gameTable.classList.remove('-finished');
 		this.gameState = new Array(9).fill(null);
 		this.gameResult.textContent = '';
-		this.currentPlayer = (Math.random() > 0.5) ? 'X' : 'O';
+		this.currentPlayer = (Math.random() > 0.5) ? PLAYER_X : PLAYER_O;
 		
 		this.clearBorde();
 		this.setCurrentPlayerName();
 	},
 	
-	clearBorde() {
-		const allCells = Array.from(document.querySelectorAll('.game__cell'));
-		
-		allCells.map(cell => {
-			cell.classList.contains('-tic') && cell.classList.remove('-tic');
-			cell.classList.contains('-tac') && cell.classList.remove('-tac');
-		});
+	this.clearBorde = function() {
+		const allCells = Array.from(gameBoard.querySelectorAll('.game__cell'));
+		allCells.map(cell => cell.classList.remove('-tac', '-tic'));
 	},
 
-	isWin() {
+	this.isWin = function() {
 		for (let option of this.winOptions) {
 			let [a, b, c] = option;
 			if (this.isOptionsEqual(this.gameState[a], this.gameState[b], this.gameState[c]))
@@ -95,14 +92,18 @@ const Game = {
 		return false
 	},
 
-	isOptionsEqual(a, b, c) {
-		if (a && b && c) return a === b && a === c && b === c;
+	this.isOptionsEqual = function(a, b, c) {
+		if (a && b && c) return a === b && b === c;
 	},
 
-	finisnigGame() {
-		this.gameResult.textContent = `${this.currentPlayer} WON!`;
+	this.finisnigGame = function() {
+		this.gameResult.textContent = `${this.currentPlayer} Won!`;
 		this.gameTable.classList.add('-finished');
 	}
 };
 
-Game.init();
+const game1 = new Game(document.querySelector('.game-1'));
+const game2 = new Game(document.querySelector('.game-2'));
+
+game1.init();
+game2.init();
